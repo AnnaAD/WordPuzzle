b470d035ddf4533a7fe7a02e3e56fba8ffa321b7
AnnaAD <anna.arpacidusseau@gmail.com>
2020-01-01 18:17:39 -0500

    Updated Frontend
    Added better typing/text detection and character deletion. Fixed bugs of typing too quickly causing errors. Added clue highlighting and clicking on clues to focus on squares. Added a timer.

--- game.js
+++ game.js
@@ -3,16 +3,35 @@
 var mode = "row"; //select mode is either row or column
 var width = 5;
 var height = 5;
-var letters = "abcde";
 
+var timerStart = Date.now();
 
 var settings = {
   autoNextLine: true
 }
 
+var time;
+
+var timer = setInterval(function() {
+
+  var now = Date.now();
+  var distance = now - timerStart;
+
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if((seconds+"").length < 2) {
+    seconds = "0"+seconds;
+  }
+
+  time = minutes + ":" + seconds;
+  $("#timer").text(time);
+  console.log("run");
+
+},1000);
 
 $(function(){
-  var oldFocus = $("#a1");
+  var oldFocus = $("#11");
   oldFocus.focus();
   updateHighlights(oldFocus);
 
@@ -24,47 +43,76 @@ $(function(){
     $("#dclue" + i).text(puzzle.downClues[i-1]);
   }
 
-  //changes focus automatically after typing a character
-  $("input").keyup(function (e) {
-    if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
-      //console.log("returned");
-      return;
-    }
-    //console.log("running");
-    if (this.value.length == this.maxLength) {
-      var id = $(this).attr('id');
-      var newfocusId;
-      if(mode == "row") {
-        newfocusId = incrementRow(id);
-      } else if (mode == "column") {
-        newfocusId = incrementColumn(id);
-      }
 
-      $("#"+newfocusId).focus();
-      oldFocus = $("#"+newfocusId);
+  $('input').on('input',function(e){
+    content = $('input').val();
 
+    console.log("change");
+    var id = $(this).attr('id');
+
+    var newfocusId;
+    console.log(id);
+    if(mode == "row") {
+      newfocusId = incrementRow(id);
+    } else if (mode == "column") {
+      newfocusId = incrementColumn(id);
     }
+
+    $("#"+newfocusId).focus();
+    console.log(newfocusId);
+    oldFocus = $("#"+newfocusId);
   });
 
+  $("li").click(function(e) {
+    var id = $(this).attr("id");
+
+    var index = id.slice(id.length-1, id.length);
+
+    var oldFocus;
+    if(id.slice(0,1) === "d") {
+      oldFocus = $("#1"+index);
+      mode = "column";
+    } else {
+      oldFocus = $("#"+index+"1");
+      mode = "row";
+    }
+    oldFocus.focus();
+    updateHighlights(oldFocus);
+  });
   //Allows for users to easily type over old inputs
   $("input").keydown(function (e) {
+    console.log("keydown");
     if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
       return;
     }
+    var id = $(this).attr('id');
+    console.log(id);
+    console.log($(this).val());
 
-    var id = $(this).attr("id");
+    var delete_next = false;
+    if($(this).val() == "") {
+      delete_next = true;
+    }
+
+    console.log(delete_next);
+
+    $(this).val("");
+
+
     //deal with delete keypress
     if(e.keyCode == 8 || e.keyCode == 46) {
       if(mode == "row") {
-        $(this).val("");
-        $("#" + id.slice(0,1) + (parseInt(id.slice(1,2)) - 1)).focus();
+        if(delete_next) {
+          $("#" + id.slice(0,1) + (parseInt(id.slice(1,2)) - 1)).focus();
+          $("#" + id.slice(0,1) + (parseInt(id.slice(1,2)) - 1)).val("");
+        }
       } else {
-        $(this).val("");
-        var index = letters.indexOf(id.slice(0,1));
-        $("#" + letters.slice(index-1, index) + id.slice(1,2)).focus();
+          if(delete_next) {
+            $("#" + (parseInt(id.slice(0,1)) - 1) + id.slice(1,2)).focus();
+            $("#" + (parseInt(id.slice(0,1)) - 1) + id.slice(1,2)).val("");
+          }
+        }
       }
-    }
-    $(this).val("");
   });
 
 
@@ -92,8 +140,7 @@ $(function(){
       }
     } else if (e.keyCode == 40) {
       if(mode == "column") {
-        var index = letters.indexOf(id.slice(0,1));
-        $("#" + letters.slice(index+1, index+2) + id.slice(1,2)).focus();
+        $("#" + (parseInt(id.slice(0,1)) + 1) + id.slice(1,2)).focus();
       } else {
         mode = "column";
         updateHighlights($(document.activeElement));
@@ -107,8 +154,7 @@ $(function(){
       }
     } else if (e.keyCode == 38) {
       if(mode == "column") {
-        var index = letters.indexOf(id.slice(0,1));
-        $("#" + letters.slice(index-1, index) + id.slice(1,2)).focus();
+        $("#" + (parseInt(id.slice(0,1)) - 1) + id.slice(1,2)).focus();
       } else {
         mode = "column";
         updateHighlights($(document.activeElement));
@@ -143,33 +189,40 @@ $(function(){
 function updateHighlights(target) {
   clearAllHighlights();
   var targetId = target.attr('id');
+  console.log(targetId);
   if(mode == "row") {
+    $("#aclue"+targetId.slice(0,1)).addClass("highlighted");
     highlightRow(targetId.slice(0,1));
   } else if (mode == "column"){
+    $("#dclue"+targetId.slice(1,2)).addClass("highlighted");
     highlightColumn(targetId.slice(1,2));
   }
 }
 
 function clearAllHighlights() {
-  for(var i = 0; i < letters.length; i++) {
+  for(var i = 1; i <= height; i++) {
+    $("#aclue"+i).removeClass("highlighted");
     for(var j = 1; j <= width; j++) {
-      var targetId = letters.slice(i,i+1) + j;
+      if(i==1) {
+        $("#dclue"+j).removeClass("highlighted");
+      }
+      var targetId = i + "" + j;
       $("#" + targetId).removeClass("highlighted");
       $("#" + targetId).removeClass("incorrect");
     }
   }
 }
 
-function highlightColumn(number) {
-  for(var i = 0; i < letters.length; i++) {
-    var targetId = letters.slice(i,i+1) + number;
+function highlightColumn(col) {
+  for(var i = 1; i <= width; i++) {
+    var targetId = i + "" + col;
     $("#" + targetId).addClass("highlighted");
   }
 }
 
-function highlightRow(letter) {
+function highlightRow(row) {
   for(var i = 1; i <= width; i++) {
-    var targetId = letter + i;
+    var targetId = row + "" + i;
     $("#" + targetId).addClass("highlighted");
   }
 }
@@ -183,29 +236,29 @@ function incrementRow(id) {
       return;
     }
   } else {
-    return id.slice(0,1) + (parseInt(id.slice(1,2)) + 1);
+    return id.slice(0,1) + ""  + (parseInt(id.slice(1,2)) + 1);
   }
 }
 
 function incrementColumn(id) {
-  var index = letters.indexOf(id.slice(0,1));
-  if(index == letters.length-1) {
+  if(parseInt(id.slice(0,1)) == height) {
     if(settings.autoNextLine) {
-      id = "a" + id.slice(1,2);
+      id = "1" + id.slice(1,2);
       return incrementRow(id);
     } else {
       return;
     }
+  } else {
+    return (parseInt(id.slice(0,1)) + 1) + "" + id.slice(1,2);
   }
-  return letters.slice(index+1,index+2) + id.slice(1,2);
 }
 
 function checkWin() {
   var win = true;
-  for(var i = 0; i < letters.length; i++) {
+  for(var i = 1; i <= height; i++) {
     for(var j = 1; j <= width; j++) {
-      var targetId = letters.slice(i,i+1) + j;
-      if($("#" + targetId).val() != puzzle.grid[i][j-1]) {
+      var targetId = i + "" + j;
+      if($("#" + targetId).val() != puzzle.grid[i-1][j-1]) {
         win = false;
         $("#" + targetId).addClass("incorrect");
       }
@@ -213,6 +266,7 @@ function checkWin() {
   }
 
   if(win) {
-    alert("Correct! You got it!");
+    alert("Correct! You got it! TIME: " + time);
+    clearInterval(timer);
   }
 }
